34094d23628dfc920d5be947b4df9264bbe5459e
santhoshshekar2004@gmail.com <jbportalsmultiservice06@gmail.com>
2023-05-04 16:24:11 +0530

    all commits done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,14 @@
-// src/index.tsx
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import { counterSlice } from '../store/counterSlice';
-import App from './App';
+const inter = Inter({ subsets: ['latin'] })
 
-const store = configureStore({
-  reducer: {
-    counter: counterSlice.reducer,
-  },
-});
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <h1 className='text-xl font-bold font-sans'> multi service app 06</h1>
+    </main>
+  )
+}
